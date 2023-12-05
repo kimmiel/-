db4ebdde630254d82a10b870e9dfcb06c6ee4574
LAPTOP-CM5DVN5T\User <kevin910504@gmail.com>
2023-12-05 20:18:23 +0800

    add function play_file()

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import tkinter as tk
 from tkinter import filedialog
 import pyaudio
-import wave
+import wave, sys, threading
 
 import pygame
 window=tk.Tk()
@@ -27,8 +27,13 @@ def upload_file():
         # pygame.mixer.music.load(path)
         # pygame.mixer.music.play()
 
+
+# 播放
+def play_file():
+    path = filedialog.askopenfilename()
+    if path:
         # use pyaudio to open a stream
-        #讀取 .wav檔
+        # 讀取 .wav檔
         wf = wave.open(path, 'rb')  # 'rb' : 二進位讀取模式。'wb' : 二進位寫入模式。'r+b' : 二進位讀寫模式。
         p = pyaudio.PyAudio()
         stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
@@ -36,12 +41,20 @@ def upload_file():
                         rate=wf.getframerate(),
                         output=True)
 
+        buf = 1024
+        while True:
+            data = wf.readframes(buf)
+            if data == '':
+                break
+            stream.write(data)
+
+
 def save_mp3():
     # wait where the output  music
     print(f" wait where the output  music")
 
 
-upload_button = tk.Button(window,background="#ea4462",fg="white",font=('Arial',14), text="upload your muise",width=50,height=4,borderwidth =0,highlightthickness=0,command=upload_file)
+upload_button = tk.Button(window,background="#ea4462",fg="white",font=('Arial',14), text="upload your muise",width=50,height=4,borderwidth =0,highlightthickness=0,command=play_file)
 save_button = tk.Button(window,background="#fbabbb",fg="white",font=('Arial',14), text="save muise",width=50,height=4,borderwidth =0,highlightthickness=0,command=save_mp3)
 button3 = tk.Button(window,background="#3e3e3e",fg="white",font=('Arial',14), text="備用button3",width=50,height=4,borderwidth =0,highlightthickness=0)
 button4 = tk.Button(window,background="#fafafa",fg="black",font=('Arial',14), text="備用button4",width=50,height=4,borderwidth =0,highlightthickness=0)
