41039386340a91e3c1117f4bff00db5b94680a18
Hsiaocccc <148777916+Hsiaocccc@users.noreply.github.com>
2023-12-05 20:22:26 +0800

    Update app.py
    add function play_file()

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
 import tkinter as tk
 from tkinter import filedialog
+import pyaudio
+import wave, sys, threading
 
 import pygame
 window=tk.Tk()
@@ -16,19 +18,43 @@ label.grid(row=1 , column=0,sticky='w',padx = 20,pady = 5)
 
 # the function of upload the musie
 
+
 def upload_file():
     path=filedialog.askopenfilename()
     if path:
-        print(f"Uplasding Muise from file: {path}")
+        print(f"Uploading Music from file: {path}")
         # pygame.mixer.init()
         # pygame.mixer.music.load(path)
         # pygame.mixer.music.play()
+
+
+# 播放
+def play_file():
+    path = filedialog.askopenfilename()
+    if path:
+        # use pyaudio to open a stream
+        # 讀取 .wav檔
+        wf = wave.open(path, 'rb')  # 'rb' : 二進位讀取模式。'wb' : 二進位寫入模式。'r+b' : 二進位讀寫模式。
+        p = pyaudio.PyAudio()
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+
+        buf = 1024
+        while True:
+            data = wf.readframes(buf)
+            if data == '':
+                break
+            stream.write(data)
+
 
 def save_mp3():
     # wait where the output  music
     print(f" wait where the output  music")
 
-upload_button = tk.Button(window,background="#ea4462",fg="white",font=('Arial',14), text="upload your muise",width=50,height=4,borderwidth =0,highlightthickness=0,command=upload_file)
+
+upload_button = tk.Button(window,background="#ea4462",fg="white",font=('Arial',14), text="upload your muise",width=50,height=4,borderwidth =0,highlightthickness=0,command=play_file)
 save_button = tk.Button(window,background="#fbabbb",fg="white",font=('Arial',14), text="save muise",width=50,height=4,borderwidth =0,highlightthickness=0,command=save_mp3)
 button3 = tk.Button(window,background="#3e3e3e",fg="white",font=('Arial',14), text="備用button3",width=50,height=4,borderwidth =0,highlightthickness=0)
 button4 = tk.Button(window,background="#fafafa",fg="black",font=('Arial',14), text="備用button4",width=50,height=4,borderwidth =0,highlightthickness=0)
